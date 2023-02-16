873b48c1b6b12ea407baa6031850bf1fcf521488
Chris Doucette <chrisdoucette15@gmail.com>
2023-02-16 12:03:26 -0500

    Update __init__.py

--- umbrella_api/__init__.py
+++ umbrella_api/__init__.py
@@ -1,3 +1,5 @@
 __author__ = "chrisdoucette15@gmai.com"
 
 from .umbrella_api import UmbrellaAPI
+from .resources import DestinationList, Destination
+from .exceptions import UmbrellaAPIError, InputValidationError
